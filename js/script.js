19d2556fae00717d2158ed396950117b7d7034dd
Sergey Okey <sergeyborisov_1@vk.com>
2023-12-19 20:14:12 +0300

    update

--- js/script.js
+++ js/script.js
@@ -8,4 +8,13 @@ function scrollFunction() {
   }
 }
 
-// slider foto
+// Плаваня прокрутка к ссылкам.
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
