7ff5b4f9efc500072056bb3a1a7f09be203c7c13
Sergey Okey <sergeyborisov_1@vk.com>
2023-12-20 15:27:23 +0300

    update

--- js/script.js
+++ js/script.js
@@ -18,3 +18,39 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+// Active Link
+document.addEventListener('scroll', function () {
+  var menuLinks = document.querySelectorAll('.menu-link');
+
+  menuLinks.forEach(function (link) {
+    var sectionId = link.getAttribute('href').substring(1);
+    var section = document.getElementById(sectionId);
+
+    if (section.offsetTop <= window.scrollY && section.offsetTop + section.offsetHeight > window.scrollY) {
+      menuLinks.forEach(function (link) {
+        link.classList.remove('active');
+      });
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+});
+
+
+// Modal Windows
+
+document.getElementById('openModal').addEventListener('click', function () {
+  document.getElementById('modal').style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function () {
+  document.getElementById('modal').style.display = 'none';
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target == document.getElementById('modal')) {
+    document.getElementById('modal').style.display = 'none';
+  }
+});
