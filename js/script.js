b5e6be3df6d75d3f83818d6940dac354236800ad
Sergey-Okey <sergeyborisov_1@vk.ru>
2023-12-23 12:38:41 +0300

    update

--- js/script.js
+++ js/script.js
@@ -23,14 +23,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // Modal Windows
 
-document.getElementById('openModal').addEventListener('click', function () {
-  document.getElementById('modal').style.display = 'block';
-});
+// Проверка, является ли устройство мобильным
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+// Обработчик события
+function toggleModal() {
+  var modal = document.getElementById('modal');
+  var modalDisplay = window.getComputedStyle(modal).getPropertyValue('display');
+
+  if (modalDisplay === 'none') {
+    modal.style.display = 'block';
+  } else {
+    modal.style.display = 'none';
+  }
+}
+
+// Добавление обработчика события в зависимости от устройства
+if (isMobile) {
+  document.getElementById('openModal').addEventListener('touchstart', toggleModal);
+} else {
+  document.getElementById('openModal').addEventListener('click', toggleModal);
+}
 
 document.getElementsByClassName('close')[0].addEventListener('click', function () {
   document.getElementById('modal').style.display = 'none';
 });
-
 // Burger Menu
 document.querySelector('.menu-toggle').addEventListener('click', function () {
   this.classList.toggle('active');
